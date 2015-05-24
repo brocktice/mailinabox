92794e66bbef40f9d1ac92f675eb3e041af80348
Brock Tice <brock@brocktice.com>
2015-05-24 16:34:15 -0400

    Move db init to separate file.

--- setup/rcmcarddav.sh
+++ setup/rcmcarddav.sh
@@ -15,10 +15,6 @@ git_clone $CARDDAVGIT master '' $CARDDAVDIR
 
 # ### Install composer
 
-# initialize roundcube database
-export PRIVATE_IP
-curl -sk https://${PRIVATE_IP}/mail/index.php 2>&1 >> /tmp/roundcube_db_init.log
-
 # We need php5-curl for this
 apt_install php5-curl
 
@@ -53,15 +49,6 @@ EOF
 # Enable plugin
 sed -ri "s@'vacation_sieve'\)@'vacation_sieve', 'carddav'\)@" $RCMCONFIG
 
-# Work around bug in db init code in rcmcarddav
-RCMSQLF=/home/user-data/mail/roundcube/roundcube.sqlite
-DBINIT=/usr/local/lib/roundcubemail/plugins/carddav/dbinit/sqlite3.sql
-DBMIG=/usr/local/lib/roundcubemail/plugins/carddav/dbmigrations/0000-dbinit/sqlite3.sql
-
-# This may fail if we've already created the database, so capture output
-/usr/bin/sqlite3 $RCMSQLF < $DBINIT &> /tmp/dbinit.log
-#/usr/bin/sqlite3 $RCMSQLF < $DBMIG &> /dev/null
-
 # Fix permissions.
 chmod -R 644 $CARDDAVDIR
 chmod -R a+X $CARDDAVDIR
@@ -74,7 +61,3 @@ chown -f -R www-data.www-data $RCMCONFIG
 
 # Should be all set after a restart
 restart_service nginx
-
-# sleep and then initialize roundcube database
-/bin/sleep 3
-curl -sk https://${PRIVATE_IP}/mail/index.php 2>&1 >> /tmp/roundcube_db_init.log
