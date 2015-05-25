b12ec891a332f8962f773a38e7f4f4313b0cd426
Brock Tice <brock@brocktice.com>
2015-05-25 08:29:04 -0600

    Switch back to my ppa so people can use this branch for testing.

--- setup/lucene.sh
+++ setup/lucene.sh
@@ -16,7 +16,10 @@ source /etc/mailinabox.conf # load global vars
 # ---------------------------------------
 
 # Add official ppa
-hide_output add-apt-repository -y ppa:mail-in-a-box/ppa
+#hide_output add-apt-repository -y ppa:mail-in-a-box/ppa
+
+# Brock's ppa for testing
+hide_output add-apt-repository -y ppa:brock/mailinabox-brocktice
 
 # Update apt
 hide_output apt-get update
